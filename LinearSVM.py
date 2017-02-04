de87538c71e865517530cb3865185f2f7ab8d9f3
Linxiao Bai <Pagliacci@MacBook-Air.local>
2017-02-04 18:47:51 -0500

    second hw sub

--- LinearSVM.py
+++ LinearSVM.py
@@ -1,15 +1,18 @@
 import numpy as np
 from sklearn.datasets import load_svmlight_file
 
-class Pla:
-    def __init__(self,maxit):
+class SVM:
+    def __init__(self, maxit=100,c=1):
         self.maxIt = maxit
         self.shape=tuple()
+        self.curit = 1
         self.error=0
         self.w=None
+        self.C=c
 
 
-    def load(self, dir,feat=123):
+
+    def load(self, dir, feat=123):
         x_train, y_train = load_svmlight_file(dir, n_features=feat)
         return (x_train, y_train)
 
@@ -20,19 +23,25 @@ class Pla:
     def train(self, dir):
         d, y=self.load(dir)
         self.shape= d.shape
-        self.w=np.reshape(np.zeros(self.shape[1]),(1,self.shape[1]))
-
         x = d.todense()
-        for i in range(self.maxIt):
+        x=np.append(x, np.ones(self.shape[0]).reshape((self.shape[0],1)),1)
+        self.shape = x.shape
+        self.w = np.reshape(np.zeros(self.shape[1]), (1, self.shape[1]))
+
+        while True:
             for j in range(self.shape[0]):
                 yhat=self.activation(int(np.dot(self.w, np.transpose(x[j]))))
-                self.w= self.w + (np.sign(y[j]-yhat) * x[j])
+                self.w= self.w + ((self.shape[0]*self.C/self.curit)*(np.sign(y[j]-yhat) * x[j])) - (2./self.curit)*self.w
+                self.curit+=1
+                if self.curit>self.maxIt: break
+            if self.curit > self.maxIt: break
 
 
     def test(self, dir):
         result=0
         x, y = self.load(dir)
         x=x.todense()
+        x=np.append(x, np.ones(x.shape[0]).reshape((x.shape[0],1)),1)
 
         for i in range(len(y)):
             yhat = self.activation(int(np.matmul(self.w, np.transpose(x[i]))))
--- Test1.py
+++ Test1.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_svmlight_file
 import numpy as np
 from Pla import Pla
+from LinearSVM import SVM
 
 x_train, y_train = load_svmlight_file("/Users/Pagliacci/Desktop/MachineLearing/a7a.train.csv", n_features=123)
 # x_test, y_test = load_svmlight_file("/Users/Pagliacci/Desktop/MachineLearing/a7a.test.csv", n_features=123)
@@ -11,7 +12,10 @@ x_train, y_train = load_svmlight_file("/Users/Pagliacci/Desktop/MachineLearing/a
 # print (np.zeros(123)+np.transpose(x_train[2])).shape
 
 
-pla=Pla(2)
-pla.train("/Users/Pagliacci/Desktop/MachineLearing/a7a.train.csv")
-print pla.test("/Users/Pagliacci/Desktop/MachineLearing/a7a.test.csv")
+# pla=Pla(2)
+# pla.train("/Users/Pagliacci/Desktop/MachineLearing/a7a.train.csv")
+# print pla.test("/Users/Pagliacci/Desktop/MachineLearing/a7a.test.csv")
 
+svm=SVM(100,c=4)
+svm.train("/Users/Pagliacci/Desktop/MachineLearing/a7a.train.csv",)
+print svm.test("/Users/Pagliacci/Desktop/MachineLearing/a7a.test.csv")
